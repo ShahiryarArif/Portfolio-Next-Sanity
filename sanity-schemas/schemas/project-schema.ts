6f62a621aa6fe934c09f46520d93601342bd8ec0
ShahiryarArif <shahiryararif@gmail.com>
2023-07-24 20:48:05 +0500

    First Sanity Schema

---
+++ sanity-schemas/schemas/project-schema.ts
@@ -0,0 +1,44 @@
+const project = {
+  name: 'project',
+  title: 'Project',
+  type: 'documnet',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'name' },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: { hotspot: true }, // This means we can zoom in and pick certain section of image to always show up
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alt',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'url',
+      title: 'URL',
+      type: 'url',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }], // If we do this that mean we can do heading/ bold etc in studio
+    },
+  ],
+};
+
+export default project;
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from "./sanity-schemas/schemas";
 
 const config = defineConfig({
     projectId:"rw69g7b2",
@@ -7,7 +8,8 @@ const config = defineConfig({
     title: "Portfolio Next Sanity Website",
     apiVersion: "2023-03-04",
     basePath: "/admin", // This is where sanity studio will live like wordpress we can do /admin
-    plugins: [deskTool()] // desktool is for top level view and is needed to view our studio
+    plugins: [deskTool()], // desktool is for top level view and is needed to view our studio
+    schema: { types: schemas },
 })
 
 export default config;
