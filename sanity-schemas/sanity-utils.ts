4ac21e8c7b1314cea4558a216d37370085f58d6f
ShahiryarArif <shahiryararif@gmail.com>
2023-07-25 15:44:34 +0500

    Setting up Typescript types (fixed project error)

--- sanity-schemas/sanity-utils.ts
+++ sanity-schemas/sanity-utils.ts
@@ -1,8 +1,10 @@
 // Contains all the functions for sanity
 
+import { Project } from '@/types/Project';
 import { createClient, groq } from 'next-sanity';
 
-export async function getProjects() {
+//Instead for declearing type everywhere when variable is created we can define the return type
+export async function getProjects(): Promise<Project[]> {
   const client = createClient({
     projectId: 'rw69g7b2',
     dataset: 'production',
