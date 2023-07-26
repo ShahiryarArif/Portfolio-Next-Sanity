affe4618bc0421a113c3c6366ba8a7790cf9549e
ShahiryarArif <shahiryararif@gmail.com>
2023-07-26 17:29:47 +0500

    Pages Schema and Project finalization

--- sanity-schemas/sanity-utils.ts
+++ sanity-schemas/sanity-utils.ts
@@ -3,6 +3,7 @@
 import { Project } from '@/types/Project';
 import { createClient, groq } from 'next-sanity';
 import clientConfig from './config/client-config';
+import { Page } from '@/types/Page';
 
 //Instead for declearing type everywhere when variable is created we can define the return type
 export async function getProjects(): Promise<Project[]> {
@@ -34,6 +35,30 @@ export async function getProject(slug: string): Promise<Project> {
         "slug": slug.current,
         "image": image.asset->url,
         url,
+        content
+    }`,
+    { slug }
+  );
+}
+
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+        _id,
+        _createdAt,
+        title,
+        "slug": slug.current,
+    }`
+  );
+}
+
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug[0]]{
+        _id,
+        _createdAt,
+        title,
+        "slug": slug.current,
         content
     }`,
     { slug }
